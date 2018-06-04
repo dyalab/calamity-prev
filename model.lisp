ec15a34077b6b4be68ae9fb4e31df17d83ca4696
Neil T. Dantam <ndantam@mines.edu>
2018-06-04 16:31:26 -0600

    Design frame

--- model.lisp
+++ model.lisp
@@ -11,6 +11,98 @@
   (load-scene-file "package://franka_description/robots/panda_arm_hand.urdf"))
 
 
-(win-set-scene-graph *panda*)
+(defparameter *l* 8e-2)
+
+(defparameter *spine-height* 1.75)
+(defparameter *shoulder-height* 1.5)
+(defparameter *spine-off* .25)
+(defparameter *plate-thickness* .1e-2)
+
+(defparameter *shoulder-len* .5)
+
+(defparameter *back* .75)
+
+
+(defparameter *back-len* (- (vec-norm  (vec *back* *spine-height*))
+                            1e-2))
+(defparameter *back-angle* (atan *back*
+                                 *spine-height*))
+
+
+(defparameter *top-vec* (vec *shoulder-len* (- *spine-height* *shoulder-height*)))
+(defparameter *top-len* (- (vec-norm *top-vec*) *l*))
+(defparameter *top-angle* (atan (elt *top-vec* 0)
+                                (elt *top-vec* 1)))
+
+(defparameter *frame-options* (draw-options-default :color '(.5 .5 .5)))
+
+(defun albox (x y z)
+  (scene-geometry-box *frame-options*
+                      (vec x y z)))
+
+(defparameter *frame*
+  (scene-graph
+
+   (scene-frame-fixed nil "foot")
+
+   (scene-frame-fixed "foot" "spine-left"
+                      :tf (tf* nil (vec 0 *spine-off* (* .5 *spine-height*)))
+                      :geometry (albox *l* *l* *spine-height*))
+   (scene-frame-fixed "foot" "plate"
+                      :tf (tf* nil nil)
+                      :geometry (albox 2 2 *plate-thickness*))
+   (scene-frame-fixed "foot" "spine-right"
+                      :tf (tf* nil (vec 0 (- *spine-off*) (* .5 *spine-height*)))
+                      :geometry (albox *l* *l* *spine-height*))
+   (scene-frame-fixed "foot" "cross-top"
+                      :tf (tf* nil (vec 0 0 (- *spine-height* (* .5 *l*))))
+                      :geometry (albox *l*
+                                       (- (* 2 *spine-off*) *l*)
+                                       *l*))
+   (scene-frame-fixed "foot" "shoulder-left"
+                      :tf (tf* nil (vec (* .5 *shoulder-len*) *spine-off* *shoulder-height*))
+                      :geometry (albox *shoulder-len* *l* *l*))
+   (scene-frame-fixed "foot" "shoulder-right"
+                      :tf (tf* nil (vec (* .5 *shoulder-len*) (- *spine-off*) *shoulder-height*))
+                      :geometry (albox *shoulder-len* *l* *l*))
+
+   (scene-frame-fixed "foot" "shoulder-top"
+                      :tf (tf* nil (vec (- *shoulder-len* (* .5 *l*)) 0 *shoulder-height* ))
+                      :geometry (albox *l*
+                                       (- (* 2 *spine-off*) *l*)
+                                       *l*))
+
+
+   (scene-frame-fixed "foot" "back-foot"
+                      :tf (tf* (y-angle *back-angle*) (vec (- *back*) 0 0)))
+
+   (scene-frame-fixed "back-foot" "back-left"
+                      :tf (tf* nil (vec 0 *spine-off*  (* .495 *back-len*)))
+                      :geometry (albox *l* *l* *back-len*))
+
+   (scene-frame-fixed "back-foot" "back-right"
+                      :tf (tf* nil (vec 0 (- *spine-off*)  (* .495 *back-len*)))
+                      :geometry (albox *l* *l* *back-len*))
+
+   (scene-frame-fixed "foot" "top-support"
+                      :tf (tf* (y-angle (- *top-angle*))
+                               (vec 0 0  (- *spine-height* (* .5 *l*)))))
+
+   (scene-frame-fixed "top-support" "top-left"
+                      :tf (tf* nil (vec 0  *spine-off* (* -.5 *top-len* )))
+                      :geometry (albox *l* *l* *top-len*))
+
+   (scene-frame-fixed "top-support" "top-right"
+                      :tf (tf* nil (vec 0  (- *spine-off*) (* -.5 *top-len* )))
+                      :geometry (albox *l* *l* *top-len*))
+
+
+
+
+   ))
+
+
+;(win-set-scene-graph *panda*)
+(win-set-scene-graph *frame*)
 
 (win-run)
